fabbca4790a300a5bab38ad661867c08128347eb
digma122012-commits <digma122012@gmail.com>
2026-02-23 23:02:58 +0300

    Update Fatal_Floors.lua

--- Fatal_Floors.lua
+++ Fatal_Floors.lua
@@ -6,7 +6,7 @@ local Tab = Window:NewTab("Farm")
 
 local Section_Farm = Tab:NewSection("Teleport")
 
-Section_Farm:NewButton("Teleport to Ore", "Teleport to Ore, PLEASE USE IT IN LOADED GAME", function()
+function TeleportToOre()
     local ProcGenGenerated = workspace:FindFirstChild("ProcGenGenerated")
     local ProcGenItems = ProcGenGenerated:FindFirstChild("ProcGenItems"):GetChildren()
 
@@ -17,22 +17,138 @@ Section_Farm:NewButton("Teleport to Ore", "Teleport to Ore, PLEASE USE IT IN LOA
         local RootPart = object:FindFirstChild("Handle")
         HRP.CFrame = RootPart.CFrame
     end 
-end)
+end
 
-Section_Farm:NewButton("Teleport to Shard", "Teleport to Shard, PLEASE USE IT IN LOADED GAME", function()
+function TeleportToSellStantion()
+    local SellingSpot = workspace.PlacedTiles.SellStation.SellingSpot
+    local Character = game.Players.LocalPlayer.Character
+    local HRP = Character.HumanoidRootPart
+
+    HRP.CFrame = SellingSpot.CFrame
+end
+
+function TeleportToShard()
     local ProcGenGenerated = workspace:FindFirstChild("ProcGenGenerated")
-    local Shard = ProcGenGenerated.Room.Shard:GetChildren()
+    if ProcGenGenerated:FindFirstChild("Room") then
+        if ProcGenGenerated.Room:FindFirstChild("Shard") then
+            if not ProcGenGenerated.Room.Shard:FindFirstChild("Shard") then
+                ProcGenGenerated:FindFirstChild("Room"):FindFirstChild("Shard"):Destroy()
+            end
+        end
+        if ProcGenGenerated:FindFirstChild("Room"):FindFirstChild("Shard") then
+
+            local Shard = ProcGenGenerated.Room:FindFirstChild("Shard").Shard
+            local Character = game.Players.LocalPlayer.Character
+            local HRP = Character.HumanoidRootPart
+            print(HRP)
+
+            HRP.CFrame = Shard.CFrame
+        end
+    end
+    
+    if ProcGenGenerated:FindFirstChild("FactoryT") then
+        if not ProcGenGenerated.FactoryT:FindFirstChild("Shard") then
+            if not ProcGenGenerated.FactoryT.Shard:FindFirstChild("Shard") then
+                ProcGenGenerated:FindFirstChild("FactoryT"):FindFirstChild("Shard"):Destroy()
+            end
+        end
+        if ProcGenGenerated:FindFirstChild("FactoryT"):FindFirstChild("Shard") then
+
+            local Shard = ProcGenGenerated.FactoryT:FindFirstChild("Shard").Shard
+            local Character = game.Players.LocalPlayer.Character
+            local HRP = Character.HumanoidRootPart
+            print(HRP)
+
+            HRP.CFrame = Shard.CFrame
+        end
+    end
+    if ProcGenGenerated:FindFirstChild("MansionLootroom") then
+        if ProcGenGenerated.MansionLootroom:FindFirstChild("Shard") then
+            if not ProcGenGenerated.MansionLootroom.Shard:FindFirstChild("Shard") then
+                ProcGenGenerated:FindFirstChild("MansionLootroom"):FindFirstChild("Shard"):Destroy()
+            end
+        end
+
+        if ProcGenGenerated:FindFirstChild("MansionLootroom"):FindFirstChild("Shard") then
+            local Shard = ProcGenGenerated.MansionLootroom:FindFirstChild("Shard").Shard
+            local Character = game.Players.LocalPlayer.Character
+            local HRP = Character.HumanoidRootPart
+            print(HRP)
+
+            HRP.CFrame = Shard.CFrame
+
+
+        end
+    end
+     if ProcGenGenerated:FindFirstChild("FactoryOffice") then
+        if ProcGenGenerated.FactoryOffice:FindFirstChild("Shard") then
+            if not ProcGenGenerated.FactoryOffice.Shard:FindFirstChild("Shard") then
+                ProcGenGenerated:FindFirstChild("FactoryOffice"):FindFirstChild("Shard"):Destroy()
+            end
+        end
+        if ProcGenGenerated:FindFirstChild("FactoryOffice"):FindFirstChild("Shard") then
+            local Shard = ProcGenGenerated.FactoryOffice:FindFirstChild("Shard").Shard
+            local Character = game.Players.LocalPlayer.Character
+            local HRP = Character.HumanoidRootPart
+            print(HRP)
+
+            HRP.CFrame = Shard.CFrame
+
+        end
+    end
+end
+
+function TeleportToElevator()
+    local Map = workspace.PlacedTiles.Map:FindFirstChild("Map")
 
     local Character = game.Players.LocalPlayer.Character
     local HRP = Character.HumanoidRootPart
 
-    if Shard ~= nil then
-        for _, object in pairs(Shard) do
-            local RootPart = object:FindFirstChild("Handle")
-            HRP.CFrame = RootPart.CFrame
-        end 
-    else
-        print("no shard")
-    end
+    HRP.CFrame = Map.CFrame
+end
 
+function DropItem()
+    scrap_drop = game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("ScrapDrop")
+    scrap_drop:FireServer()
+end
+
+function FarmAllOres()
+    local items = workspace.ProcGenGenerated.ProcGenItems:GetChildren()
+    if items ~= nil then
+        print(items)
+        for _, ore in pairs(items) do
+
+            local player = game.Players.LocalPlayer
+            local character = player.Character
+            local backpack = player.Backpack
+
+            TeleportToOre()
+            print("1")
+            task.wait(0.3)
+
+            TeleportToSellStantion()
+            print("2")
+
+            task.wait(1)
+        end
+        return
+    end
+end
+
+
+
+Section_Farm:NewButton("Teleport to Ore", "Teleport to Ore, PLEASE USE IT IN LOADED GAME", function()
+    TeleportToOre()
+end)
+
+Section_Farm:NewButton("Teleport to Shard", "Teleport to Shard, PLEASE USE IT IN LOADED GAME", function()
+    TeleportToShard()
+end)
+
+Section_Farm:NewButton("Teleport to Elevator", "Teleport to elevator, PLEASE USE IT IN LOADED GAME", function()
+    TeleportToElevator()
+end)
+
+Section_Farm:NewButton("Farm all ores and sell them", "Farming all ores and tp it to elevator, PLEASE USE IT IN LOADED GAME", function()
+    FarmAllOres()
 end)
